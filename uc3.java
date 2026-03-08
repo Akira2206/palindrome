cfccd33974ac42c6b801c79f7ef5941a4a0a8753
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-03-08 19:33:22 +0530

    Add Palindrome Check Using String Reverse

---
+++ uc3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String reverse = "";
+
+ 
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reverse = reverse + original.charAt(i);
+        }
+
+        // Comparing original and reversed string
+        if (original.equals(reverse)) {
+            System.out.println("The string is a Palindrome");
+        } else {
+            System.out.println("The string is NOT a Palindrome");
+        }
+
+        sc.close();
+    }
+}
